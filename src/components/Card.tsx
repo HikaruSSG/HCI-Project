55b199efd81ec270a34e45c512ff8431a591bf65
HikaruSSG <5PB25QbOcJQnUWT_h0o9O6tOkN5CsuU@protonmail.com>
2025-03-08 16:30:19 +0000

    2025-03-08

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
+import React from "react";
 import { FaMapLocationDot } from "react-icons/fa6";
 import { SlSizeFullscreen } from "react-icons/sl";
 import { LuPhilippinePeso } from "react-icons/lu";
 import { BsFillInfoSquareFill } from "react-icons/bs";
 import { SiHomeassistantcommunitystore } from "react-icons/si";
+import Link from "next/link";
 
 const cardData = [
-  {title:"Dog Room",image:"https://picsum.photos/id/237/300",price:1500000,location:"New York",floorArea:"100/m2",information:"4 bedrooms, 2 bathrooms, parking space"},
-  {title:"Condo",image:"https://picsum.photos/id/238/300",price:1500000,location:"New York",floorArea:"100/m2",information:"4 bedrooms, 2 bathrooms, parking space"},
-  {title:"Garden",image:"https://picsum.photos/id/239/300",price:1500000,location:"New York",floorArea:"100/m2",information:"4 bedrooms, 2 bathrooms, parking space"}
+  { title: "Dog Room", image: "https://picsum.photos/id/237/300", price: 1500000, location: "New York", floorArea: "100/m2", information: "4 bedrooms, 2 bathrooms, parking space" },
+  { title: "Condo", image: "https://picsum.photos/id/238/300", price: 1500000, location: "New York", floorArea: "100/m2", information: "4 bedrooms, 2 bathrooms, parking space" },
+  { title: "Garden", image: "https://picsum.photos/id/239/300", price: 1500000, location: "New York", floorArea: "100/m2", information: "4 bedrooms, 2 bathrooms, parking space" },
 ];
 
 interface CardProps {
@@ -22,54 +23,63 @@ interface CardProps {
   index?: number;
 }
 
-import Link from 'next/link';
-
 const CardProps = ({ image, price, location, floorArea, information, title }: CardProps) => {
-  // Assuming 'index' is passed as a prop to CardProps
   return (
-    <Link href={{ pathname: '/contact', query: { unit: title } }} passHref>
-  <div className="border border-primary shadow-text rounded-lg mx-4 text-text cursor-pointer">
-    <h1 className="text-3xl p-2 text-center flex items-center justify-center text-primary">
-      <SiHomeassistantcommunitystore className="mr-1" />
-      {title}
-    </h1>
-    <div className="m-4">
-      <img src={image} alt="Card Image" className="w-full h-48 object-cover rounded-md" />
-    </div>
-    <div className="m-4">
-      <div className="flex items-center mb-1 text-primary justify-center text-2xl">
-        <LuPhilippinePeso className="mr-1" />
-        <span>{price}</span>
+    <Link href={{ pathname: "/contact", query: { unit: title } }} passHref>
+      <div className="bg-background border border-primary shadow-lg rounded-lg mx-4 text-gray-800 cursor-pointer p-6 hover:shadow-xl hover:scale-105 transition duration-300">
+        <h1 className="text-3xl font-bold text-center flex items-center justify-center text-primary mb-3">
+          <SiHomeassistantcommunitystore className="mr-2 text-2xl" />
+          {title}
+        </h1>
+        <div className="mb-4">
+          <img src={image} alt="Card Image" className="w-full h-48 object-cover rounded-lg shadow-md" />
+        </div>
+        <div className="border-b-2 border-primary my-2"></div>
+        <div className="mb-4">
+          <table className="w-full">
+            <tbody>
+              <tr>
+                <td className="py-2 px-4 border-b border-primary text-primary font-semibold">
+                  <LuPhilippinePeso className="inline-block mr-2 text-xl" /> Price
+                </td>
+                <td className="py-2 px-4 border-b border-primary text-primary">{price}</td>
+              </tr>
+              <tr>
+                <td className="py-2 px-4 border-b border-primary text-primary font-semibold">
+                  <FaMapLocationDot className="inline-block mr-2 text-xl" /> Location
+                </td>
+                <td className="py-2 px-4 border-b border-primary text-primary">{location}</td>
+              </tr>
+              <tr>
+                <td className="py-2 px-4 border-b border-primary text-primary font-semibold">
+                  <SlSizeFullscreen className="inline-block mr-2 text-xl" /> Floor Area
+                </td>
+                <td className="py-2 px-4 border-b border-primary text-primary">{floorArea}</td>
+              </tr>
+              <tr>
+                <td className="py-2 px-4 text-primary font-semibold">
+                  <BsFillInfoSquareFill className="inline-block mr-2 text-xl" /> Information
+                </td>
+                <td className="py-2 px-4 text-primary">{information}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div className="flex items-center mb-1 text-primary">
-        <FaMapLocationDot className="mr-1" />
-        <span>{location}</span>
-      </div>
-      <div className="flex items-center mb-1 text-primary">
-        <SlSizeFullscreen className="mr-1" />
-        <span>{floorArea}</span>
-      </div>
-      <div className="flex items-center text-primary">
-        <BsFillInfoSquareFill className="mr-1" />
-        <span>{information}</span>
-      </div>
-    </div>
-  </div>
-</Link>
+    </Link>
   );
-}
-
+};
 
 const Card = () => {
   return (
-    <div className='grid xl:grid-cols-3 m-6 md:grid-cols-2'>
-      {cardData.map((card, index) => (
-        <CardProps key={index} image={card.image} price={card.price} location={card.location} floorArea={card.floorArea} information={card.information} title={card.title}/>
-      ))}
+    <div className="flex justify-center items-center">
+      <div className="grid xl:grid-cols-3 m-6 md:grid-cols-2">
+        {cardData.map((card, index) => (
+          <CardProps key={index} image={card.image} price={card.price} location={card.location} floorArea={card.floorArea} information={card.information} title={card.title} />
+        ))}
+      </div>
     </div>
   );
 };
 
-
-
-export default Card
+export default Card;
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,19 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Contact = () => {
-  const [email, setEmail] = useState('');
-  const [unit, setUnit] = useState('');
-  const [comment, setComment] = useState('');
+  const [date, setDate] = useState("");
+  const [location, setLocation] = useState("");
+  const [email, setEmail] = useState("");
+  const [messenger, setMessenger] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [unit, setUnit] = useState("");
+  const [businessType, setBusinessType] = useState("");
   const router = useRouter();
 
   useEffect(() => {
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: true,
+    });
+    setDate(formattedDate);
+
     if (router.query.unit) {
       setUnit(router.query.unit as string);
     }
   }, [router.query.unit]);
 
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
 
@@ -26,24 +42,120 @@ const Contact = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
+        Accept: "application/json",
       },
-      body: json
+      body: json,
     });
     const result = await response.json();
     if (response.status === 200 && result.success) {
-      window.location.href = '/thank-you';
+      window.location.href = "/thank-you";
     }
-  }
-
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-background">
       <div className="container mx-auto bg-background shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h2 className=" text-primary text-2xl font-bold mb-6">Contact Us</h2>
+        <h2 className="text-primary text-2xl font-bold mb-6">Contact Us</h2>
         <form onSubmit={handleSubmit}>
+          <div className="md:flex md:gap-4">
+            <div className="mb-4 md:w-1/4">
+              <label
+                className="block text-primary text-sm font-bold mb-2"
+                htmlFor="firstName"
+              >
+                First Name
+              </label>
+              <input
+                className="shadow appearance-none border border-primary rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-primary bg-transparent"
+                id="firstName"
+                name="firstName"
+                type="text"
+                placeholder="First Name"
+              />
+            </div>
+            <div className="mb-4 md:w-1/4">
+              <label
+                className="block text-primary text-sm font-bold mb-2"
+                htmlFor="middleName"
+              >
+                Middle Name
+              </label>
+              <input
+                className="shadow appearance-none border border-primary rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-primary bg-transparent"
+                id="middleName"
+                name="middleName"
+                type="text"
+                placeholder="M"
+                maxLength={1}
+              />
+            </div>
+            <div className="mb-4 md:w-1/4">
+              <label
+                className="block text-primary text-sm font-bold mb-2"
+                htmlFor="lastName"
+              >
+                Last Name
+              </label>
+              <input
+                className="shadow appearance-none border border-primary rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-primary bg-transparent"
+                id="lastName"
+                name="lastName"
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
+            <div className="mb-4 md:w-1/4">
+              <label
+                className="block text-primary text-sm font-bold mb-2"
+                htmlFor="age"
+              >
+                Age
+              </label>
+              <input
+                className="shadow appearance-none border border-primary rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-primary bg-transparent"
+                id="age"
+                name="age"
+                type="number"
+                placeholder="Age"
+              />
+            </div>
+          </div>
           <div className="mb-4">
-            <label className="block text-primary text-sm font-bold mb-2" htmlFor="email">
+            <label className="hidden" htmlFor="date">
+              Date
+            </label>
+            <input
+              className="hidden"
+              id="date"
+              name="date"
+              type="text"
+              placeholder="Date"
+              value={date}
+              readOnly
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-primary text-sm font-bold mb-2"
+              htmlFor="location"
+            >
+              Location
+            </label>
+            <input
+              className="shadow appearance-none border border-primary rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-primary bg-transparent"
+              id="location"
+              name="location"
+              type="text"
+              placeholder="Location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-primary text-sm font-bold mb-2"
+              htmlFor="email"
+            >
               Email
             </label>
             <input
@@ -57,23 +169,81 @@ const Contact = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block text-primary text-sm font-bold mb-2" htmlFor="unit">
+            <label
+              className="block text-primary text-sm font-bold mb-2"
+              htmlFor="messenger"
+            >
+              Messenger
+            </label>
+            <input
+              className="shadow appearance-none border border-primary rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-primary bg-transparent"
+              id="messenger"
+              name="messenger"
+              type="text"
+              placeholder="Messenger"
+              value={messenger}
+              onChange={(e) => setMessenger(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-primary text-sm font-bold mb-2"
+              htmlFor="contactNumber"
+            >
+              Contact Number
+            </label>
+            <input
+              className="shadow appearance-none border border-primary rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-primary bg-transparent"
+              id="contactNumber"
+              name="contactNumber"
+              type="tel"
+              placeholder="Contact Number"
+              value={contactNumber}
+              onChange={(e) => setContactNumber(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-primary text-sm font-bold mb-2"
+              htmlFor="unit"
+            >
               Unit
             </label>
-            <select
-              className="shadow appearance-none border border-primary rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-primary bg-transparent"
-              id="unit"
-              name="unit"
-              value={unit}
-              onChange={(e) => setUnit(e.target.value)}
-            >
+<select
+  className="shadow appearance-none border border-primary rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-primary bg-[var(--background)]"
+  id="unit"
+  name="unit"
+  value={unit}
+  onChange={(e) => setUnit(e.target.value)}
+>
               <option>Dog Room</option>
               <option>Condo</option>
               <option>Garden</option>
             </select>
           </div>
-          <div className="mb-6">
-            <label className="block text-primary text-sm font-bold mb-2" htmlFor="comment">
+
+          <div className="mb-4">
+            <label
+              className="block text-primary text-sm font-bold mb-2"
+              htmlFor="businessType"
+            >
+              Business Type
+            </label>
+            <input
+              className="shadow appearance-none border border-primary rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-primary bg-transparent"
+              id="businessType"
+              name="businessType"
+              type="text"
+              placeholder="Business Type"
+              value={businessType}
+              onChange={(e) => setBusinessType(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-primary text-sm font-bold mb-2"
+              htmlFor="comment"
+            >
               Comment
             </label>
             <textarea
@@ -81,9 +251,7 @@ const Contact = () => {
               id="comment"
               name="comment"
               placeholder="Comment"
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-            />
+            ></textarea>
           </div>
           <div className="flex items-center justify-between">
             <button
