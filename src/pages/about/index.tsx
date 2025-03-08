9926bdd092ab9dffe19bcfa77c173c4b14c06886
HikaruSSG <5PB25QbOcJQnUWT_h0o9O6tOkN5CsuU@protonmail.com>
2025-03-08 18:19:10 +0000

    2025-03-08

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function index() {
   return (
-    <div className="bg-background py-12 h-screen">
+    <div className="bg-background h-[100svh]">
       <div className="container mx-auto px-4">
         <h1 className="text-3xl font-bold mb-4">About Us</h1>
         <p className="text-text">
